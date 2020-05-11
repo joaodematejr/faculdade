43b53f75b469fb18c7ec845ba905dd108f0d7fba
joaodematejr <joaodematejr@gmail.com>
2020-05-11 01:21:13 -0300

    Atualização

--- Estruturas de Dados/Aula04/C/aula04/main.c
+++ Estruturas de Dados/Aula04/C/aula04/main.c
@@ -8,28 +8,53 @@
 
 #include <stdio.h>
 
+int exibir_tela() {
+    printf("vai tomar no cu \n");
+    return 0;
+}
+
 int main(int argc, const char * argv[]) {
+    
+    int lista_codigo_produto[5];
+    int lista_preco_produto[5];
+    int lista_preco_produto_desconto[5];
+    int lista_quantidade_estoque[5];
+    
     int limiteMaximo = 1;
     int posicao = 0;
-    int codigo_produto[4];
-    int preco_produto[4];
-    int preco_produto_desconto[4];
-    int quantidade_estoque[4];
+    int porcetagem;
 
-    
     while (posicao < limiteMaximo) {
         int codigo;
         int preco;
         int quantidade;
         printf("Digite o codigo do produto : ");
         scanf("%d", & codigo);
+        lista_codigo_produto[posicao] = codigo;
         printf("Digite o preco do produto : ");
         scanf("%d", & preco);
+        lista_preco_produto[posicao] = preco;
         printf("Digite a quantidade de produto em estoque : ");
         scanf("%d", & quantidade);
-
-        
+        lista_quantidade_estoque[posicao] = quantidade;
         posicao++;
+    }
+    
+    printf("Digite a porcetagem a aplicar na lista : ");
+    scanf("%d", & porcetagem);
+    int loop;
+    for (int loop = 0; loop < 5; ++loop) {
+        int formula = porcetagem * lista_preco_produto[loop];
+        int resultado = formula / 100;
+        lista_preco_produto_desconto[loop] = resultado;
+        for (int loop = 0; loop < 5; ++loop) {
+            if (lista_quantidade_estoque[loop] > 500) {
+                exibir_tela("Quantidade em Estoque");
+                exibir_tela("Codigo do produto");
+                exibir_tela("Preço com Desconto");
+            }
+           ;
+        }
     }
 
     return 0;
