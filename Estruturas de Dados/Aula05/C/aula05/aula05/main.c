89c40078eba4ec5ef77c9798e149c50d7c992eb1
joaodematejr <joaodematejr@gmail.com>
2020-05-19 18:47:57 -0300

    Inicio do projeto aula 05

---
+++ Estruturas de Dados/Aula05/C/aula05/aula05/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  aula05
+//
+//  Created by João Dematé Jr 🤠 on 19/05/20.
+//  Copyright © 2020 João Dematé Jr 🤠. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
